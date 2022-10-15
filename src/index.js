9a8b369970ddb42cf8a0e7a91f232fd09a1097d6
UmerZia <omerzia73@gmail.com>
2022-10-15 17:58:30 +0500

    Add render function for todo task

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './styles.css';
 
+let listContainer = document.querySelector(".todoList");
+let listOfTodo = ["microverse", "football", "Hockey"]
+
+const render = () => {
+    listOfTodo.forEach(todo => {
+        const listElement = `<li class="todoList__item">${todo}</li>`
+        listContainer.insertAdjacentHTML("beforeend", listElement);
+    })
+}
+
+render()
