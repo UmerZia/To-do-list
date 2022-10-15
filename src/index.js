05bbbaf22e6a3e55c476573a930542c0f2f52b4e
UmerZia <omerzia73@gmail.com>
2022-10-15 20:06:37 +0500

    Add add todo task functionality

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import './styles.css';
-
 const listContainer = document.querySelector('.todoList');
-const listOfTodo = [
-  {
-    id: 1,
-    status: 'true',
-    task: 'sar dubana',
-  },
-  {
-    id: 2,
-    status: 'true',
-    task: 'sar dubana',
-  },
-  {
-    id: 3,
-    status: 'true',
-    task: 'sar dubana',
-  },
-];
+const newList = document.getElementById("inputField");
+const listOfTodo = [];
 
 const render = () => {
+  clearList(listContainer)
   listOfTodo.forEach((todo) => {
     const listElement = `<li class="todoList__item">${todo.task}</li>`;
     listContainer.insertAdjacentHTML('beforeend', listElement);
   });
 };
+
+newList.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    const listName = newList.value;
+    if (listName == null || listName === "") {return}
+    const list = createList(listName);
+    newList.value = null
+    listOfTodo.push(list)
+    render()
+  }
+})
+
+const clearList = (list) => {
+  while(list.firstChild){
+    list.removeChild(list.firstChild)
+  }
+}
+
+const createList = (name) => {
+  return {id: Date.now().toString(), task:name, status: "false"}
+}
+
 
 render();
