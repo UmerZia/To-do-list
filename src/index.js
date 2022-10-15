c68b673878768773d78db0eb813108740f11e2b3
UmerZia <omerzia73@gmail.com>
2022-10-15 18:33:59 +0500

    Fix linter issue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import './styles.css';
 
-let listContainer = document.querySelector(".todoList");
-let listOfTodo = ["microverse", "football", "Hockey"]
+const listContainer = document.querySelector('.todoList');
+const listOfTodo = [
+  {
+    id: 1,
+    status: 'true',
+    task: 'sar dubana',
+  },
+  {
+    id: 2,
+    status: 'true',
+    task: 'sar dubana',
+  },
+  {
+    id: 3,
+    status: 'true',
+    task: 'sar dubana',
+  },
+];
 
 const render = () => {
-    listOfTodo.forEach(todo => {
-        const listElement = `<li class="todoList__item">${todo}</li>`
-        listContainer.insertAdjacentHTML("beforeend", listElement);
-    })
-}
+  listOfTodo.forEach((todo) => {
+    const listElement = `<li class="todoList__item">${todo.task}</li>`;
+    listContainer.insertAdjacentHTML('beforeend', listElement);
+  });
+};
 
-render()
+render();
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    bundle: path.resolve(__dirname, "src/index.js"),
+    bundle: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, 'dist'),
     },
     port: 8081,
     open: true,
@@ -22,28 +22,28 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
+      template: './src/index.html',
+      filename: 'index.html',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.svg$/,
-        use: "svg-inline-loader",
+        use: 'svg-inline-loader',
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.svg$/,
-        loader: "svg-inline-loader",
+        loader: 'svg-inline-loader',
       },
     ],
   },
