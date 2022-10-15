49854709f2996bb30b0952672c2b0969fcce44dd
UmerZia <omerzia73@gmail.com>
2022-10-16 03:42:48 +0500

    fix linter error

--- src/index.js
+++ src/index.js
@@ -1,123 +1,121 @@
-import _, { xor } from 'lodash'
-import './styles.css'
-const listContainer = document.querySelector('.todoList')
-const newList = document.getElementById("inputField")
-const addBtn = document.getElementsByClassName("wrapper__addBtn")[0]
-const LOCAL_STORAGE_LIST_KEY = 'task.listOfTodo'
-let listOfTodo = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-const listOfTodoListItem = document.querySelectorAll("todoList__item-name")
-const BtnClear = document.getElementById("BtnClear")
-import listElement from "./modules/listitem"
-BtnClear.addEventListener("click", e=>{
-  listOfTodo=listOfTodo.filter(todo=> !todo.isCompleted);
-  
-  saveAndRender();
-})
-//Reset Indexes
+import './styles.css';
+import listElement from './modules/listitem.js';
+
+const listContainer = document.querySelector('.todoList');
+const newList = document.getElementById('inputField');
+const addBtn = document.getElementsByClassName('wrapper__addBtn')[0];
+const LOCAL_STORAGE_LIST_KEY = 'task.listOfTodo';
+let listOfTodo = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+const BtnClear = document.getElementById('BtnClear');
+
+// Reset Indexes
 const resetIndex = () => {
   let index = 1;
-  listOfTodo.forEach(item => {
+  listOfTodo.forEach((item) => {
     item.id = index;
     index += 1;
-  })
-}
+  });
+};
 
-//Save data to local storage
+// Reset Editing
+const resetEditing = () => {
+  listOfTodo.forEach((todo) => {
+    todo.isEditing = false;
+  });
+};
+
+// Refresh the list when new item added
+const clearList = (list) => {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
+
+// Save data to local storage
 const save = () => {
-  resetIndex()
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(listOfTodo))
-}
+  resetIndex();
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(listOfTodo));
+};
 
-//Delete Element
+// Render item on screen
+const render = () => {
+  clearList(listContainer);
+  listOfTodo.forEach((todo) => {
+    listContainer.insertAdjacentHTML('beforeend', listElement(todo));
+  });
+};
+
+// Save date to local Storage
+const saveAndRender = () => {
+  save();
+  render();
+};
+
+// Delete Element
 window.deleteElement = (todoId) => {
-  listOfTodo = listOfTodo.filter(todo => todo.id !== todoId)
-  saveAndRender()
-}
+  listOfTodo = listOfTodo.filter((todo) => todo.id !== todoId);
+  saveAndRender();
+};
 
-//Edit Element
+// Create Task
+const createList = (name) => ({
+  id: (listOfTodo[listOfTodo.length - 1]?.id + 1) ?? 1,
+  task: name,
+  status: false,
+  isEditing: false,
+  isCompleted: false,
+});
+
+// Add item to the list
+const addListItem = () => {
+  const listName = newList.value;
+  if (listName == null || listName === '') { return; }
+  const list = createList(listName);
+  newList.value = null;
+  listOfTodo.push(list);
+  saveAndRender();
+};
+
+// Edit Element
 window.editElement = (todoId) => {
   resetEditing();
-  listOfTodo.find(todo => todo.id === todoId).isEditing=true
-  saveAndRender()
-}
+  listOfTodo.find((todo) => todo.id === todoId).isEditing = true;
+  saveAndRender();
+};
 
-//OnChange
+// OnChange
 window.updateElement = (todoId) => {
-  const task = document.getElementById(`io-${todoId}`).value
-  listOfTodo.find(todo => todo.id === todoId).task = task;
-}
+  const task = document.getElementById(`io-${todoId}`).value;
+  listOfTodo.find((todo) => todo.id === todoId).task = task;
+};
 
-//Enter value on enter key
-listOfTodoListItem.forEach((list,index) => {
-  list.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      console.log(event)
-    }
-  })
-})
-window.finishEditingElement= ()=>
-{
+window.finishEditingElement = () => {
   resetEditing();
   saveAndRender();
-}
-//Reset Editing
-const resetEditing=()=>{
-  listOfTodo.forEach(todo => {
-    todo.isEditing = false
-  })
-}
-//Mark Completed/UnCompleted
-window.ToggleCompleted =(todoId)=> {
-  const Completed=document.getElementById(`cb-${todoId}`).checked;
-  listOfTodo.find(todo => todo.id === todoId).isCompleted = Completed;
+};
+// Mark Completed/UnCompleted
+window.ToggleCompleted = (todoId) => {
+  const Completed = document.getElementById(`cb-${todoId}`).checked;
+  listOfTodo.find((todo) => todo.id === todoId).isCompleted = Completed;
   saveAndRender();
-}
-//Render item on screen
-const render = () => {
-  clearList(listContainer)
-  listOfTodo.forEach((todo) => {
-    listContainer.insertAdjacentHTML('beforeend', listElement(todo))
-  })
-}
+};
 
-//Save date to local Storage
-const saveAndRender = () => {
-  save()
-  render()
-}
-
-//Add item on pressing enter
-newList.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    addListItem()
+// Add item on pressing enter
+newList.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addListItem();
   }
-})
+});
 
-//Add item on clicking Add button
-addBtn.addEventListener("click", () => {
-  addListItem()
-})
+// Add item on clicking Add button
+addBtn.addEventListener('click', () => {
+  addListItem();
+});
 
-//Add item to the list
-const addListItem = () => {
-  const listName = newList.value
-  if (listName == null || listName === "") {return}
-  const list = createList(listName)
-  newList.value = null
-  listOfTodo.push(list)
-  saveAndRender()
-}
+render();
 
-//Refresh the list when new item added
-const clearList = (list) => {
-  while(list.firstChild){
-    list.removeChild(list.firstChild)
-  }
-}
+BtnClear.addEventListener('click', () => {
+  listOfTodo = listOfTodo.filter((todo) => !todo.isCompleted);
 
-//Create Task
-const createList = (name) => {
-  return {id: (listOfTodo[listOfTodo.length-1]?.id+1)??1, task:name, status: false, isEditing: false, isCompleted:false}
-}
-
-render()
+  saveAndRender();
+});
