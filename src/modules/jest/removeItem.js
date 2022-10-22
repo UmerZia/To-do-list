3e364d42cd649f2f66e3b0e4ce5c5ea7139c54bf
UmerZia <omerzia73@gmail.com>
2022-10-22 13:14:13 +0500

    Fix linter error

--- src/modules/jest/removeItem.js
+++ src/modules/jest/removeItem.js
@@ -1,35 +1,35 @@
 let listOfTodo = [
   {
     id: 1,
-    task: "Complete week 3",
+    task: 'Complete week 3',
     status: false,
     isEditing: false,
     isCompleted: false,
   },
   {
     id: 2,
-    task: "Complete week 4",
+    task: 'Complete week 4',
     status: false,
     isEditing: false,
     isCompleted: false,
   },
   {
     id: 3,
-    task: "Complete week 5",
+    task: 'Complete week 5',
     status: false,
     isEditing: false,
     isCompleted: false,
   },
-  
+
 ];
 
 // Delete Element
-deleteElement = (todoId) => {
+const deleteElement = (todoId) => {
   listOfTodo = listOfTodo.filter((todo) => todo.id !== todoId);
 };
 
 // Getter
-getData = () => listOfTodo;
+const getData = () => listOfTodo;
 
 module.exports = deleteElement;
 module.exports = getData;
